41aabc041fd1e00ddf3952c430263abb09fa3a22
Jorgearellano1 <129127690+Jorgearellano1@users.noreply.github.com>
2025-06-07 12:08:29 -0500

    new features

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ import json
 
 def main(context):
     try:
-        data = context.req.json
+        body = context.req.body
+        data = json.loads(body)
         prompt = data.get("prompt", "")
 
         return context.res.json({
             "status": 200,
-            "output": f"Recibido correctamente el prompt: {prompt}"
+            "output": f" Recibido correctamente el prompt: {prompt}"
         })
     except Exception as e:
         return context.res.json({
             "status": 500,
             "error": str(e)
         }, 500)
+
